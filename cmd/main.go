90b148a89d878d5fdf8d16a34009f1678b405dd6
evan <281406674@qq.com>
2018-08-14 16:37:16 +0800

    first commit
    Signed-off-by: evan <281406674@qq.com>

---
+++ cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+
+	proxy "openssl-channel-proxy"
+)
+
+var (
+	localAddrPtr  = flag.String("l", ":8000", "local address")
+	remoteAddrPtr = flag.String("r", "10.6.250.54:8822", "remote address")
+	rcertFile = flag.String("r-cert", "/opt/conf/sdk.crt", "A PEM eoncoded certificate file.")
+	rkeyFile  = flag.String("r-key", "/opt/conf/sdk.key", "A PEM encoded private key file.")
+	rcaFile   = flag.String("r-ca", "/opt/conf/ca.crt", "A PEM eoncoded ca's certificate file.")
+)
+
+func main() {
+	flag.Parse()
+
+	laddr, err := net.ResolveTCPAddr("tcp", *localAddrPtr)
+	if err != nil {
+		log.Fatalf("Failed to resolve local address: %s\n", err)
+	}
+
+	raddr, err := net.ResolveTCPAddr("tcp", *remoteAddrPtr)
+	if err != nil {
+		log.Fatalf("Failed to resolve remote address: %s\n", err)
+	}
+
+	listener, err := net.ListenTCP("tcp", laddr)
+	if err != nil {
+		log.Fatalf("Failed to open local port: %s\n", err)
+	}
+
+	// Setup a proxy for each incomming connection
+	for {
+		conn, err := listener.AcceptTCP()
+		if err != nil {
+			log.Printf("Failed to accept connection: %s\n", err)
+			continue
+		}
+
+		log.Printf("New connection from: %s\n", conn.RemoteAddr())
+		p := proxy.New(conn, raddr, rkeyFile, rcertFile, rcaFile)
+		go p.Start()
+	}
+}
---
+++ openssl_proxy.go
@@ -0,0 +1,144 @@
+package openssl_channel_proxy
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"net"
+	"github.com/spacemonkeygo/openssl"
+	"sync"
+)
+
+const bufferSize = 0xffff
+
+func prettyBytes(b uint64) string {
+	var unit uint64 = 1024
+	pre := [...]string{"k", "M", "G", "T", "P", "E"}
+	if b < unit {
+		return fmt.Sprintf("%v B", b)
+	}
+	exp := int(math.Log(float64(b)) / math.Log(float64(unit)))
+	prefix := pre[exp-1]
+	bytes := float64(b) / math.Pow(float64(unit), float64(exp))
+	return fmt.Sprintf("%.1f %sB", bytes, prefix)
+}
+
+// Proxy
+type Proxy struct {
+	sentBytes     uint64
+	receivedBytes uint64
+	rkey          *string
+	rcert         *string
+	rca           *string
+	raddr         *net.TCPAddr
+	lconn  		  *net.TCPConn
+	rconn         *openssl.Conn
+	mux           sync.Mutex //protect erred
+	erred         bool
+	done          chan bool //signal when one end of the proxy closes/errors
+}
+
+// New creates a new Proxy instance
+func New(lconn *net.TCPConn, raddr *net.TCPAddr, rkey, rcert, rca *string) *Proxy {
+	return &Proxy{
+		lconn: lconn,
+		raddr: raddr,
+		erred: false,
+		rkey:  rkey,
+		rcert: rcert,
+		rca:   rca,
+		done:  make(chan bool),
+	}
+}
+
+/*
+ * Start will open a connection to raddr
+ * and wire up the connections
+ */
+func (p *Proxy) Start() {
+	defer p.lconn.Close()
+
+	// Setup the rconn
+	var err error
+	ctx, err := openssl.NewCtxFromFiles(*p.rcert, *p.rkey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ctx.LoadVerifyLocations(*p.rca, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	p.rconn, err = openssl.Dial("tcp", p.raddr.String(), ctx, openssl.InsecureSkipHostVerification)
+	if err != nil {
+		log.Printf("Connection failed: %s\n", err)
+		p.stats()
+		return
+	}
+	defer p.rconn.Close()
+
+	// Setup the pipes
+	go p.pipe(p.lconn, p.rconn)
+	go p.pipe(p.rconn, p.lconn)
+
+	// Block until one side of the connection closes/errors
+	<-p.done
+	p.stats()
+}
+
+// stats is called when the connection closes and logs some info
+func (p *Proxy) stats() {
+	log.Printf("Proxy connection closed (%s -> %s) sent: %s received: %s\n",
+		p.lconn.RemoteAddr(), p.raddr, prettyBytes(p.sentBytes),
+		prettyBytes(p.receivedBytes))
+
+}
+
+// err is called when either lconn or rconn produce an error on read/write
+func (p *Proxy) err(s string, err error) {
+	// Make sure only one goroutine can cause the Proxy shutdown flow
+	p.mux.Lock()
+	defer p.mux.Unlock()
+
+	if p.erred {
+		return
+	}
+
+	// EOF means connection was closed -- log anything else
+	if err != io.EOF {
+		log.Printf("Connection error: %s\n", err)
+	}
+
+	p.done <- true
+	p.erred = true
+}
+
+// use io.ReadWriteCloser interface for net.TCPConn and openssl.Conn
+func (p *Proxy) pipe(src io.ReadWriteCloser, dst io.ReadWriteCloser) {
+	// Keep track of bytes sent vs received
+	isLocal := src == p.lconn
+
+	buf := make([]byte, bufferSize)
+
+	for {
+		n, err := src.Read(buf)
+		if err != nil {
+			p.err("Read error '%s'\n", err)
+			return
+		}
+
+		b := buf[:n]
+
+		n, err = dst.Write(b)
+		if err != nil {
+			p.err("Write error '%s'\n", err)
+			return
+		}
+
+		if isLocal {
+			p.sentBytes += uint64(n)
+		} else {
+			p.receivedBytes += uint64(n)
+		}
+	}
+}
